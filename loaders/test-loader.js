68b27100ce775451576f60bfb83ace88845e7037
Curry <zhonggl@meix.com>
2022-01-26 14:17:38 +0800

    ok

--- loaders/test-loader.js
+++
@@ -1,9 +0,0 @@
-var loaderUtils = require('loader-utils');
-module.exports = function (sourceCode){
-    console.log(sourceCode);
-
-    return `var dom = document.getElementsByTagName('head')[0];
-    var styleDom = document.createElement('style');
-    styleDom.innerHTML = \`${sourceCode}\`;
-    dom.appendChild(styleDom)`
-}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-require('./assets/index.css')
+console.log('1')
+console.log('2')
+console.log('3')
+console.log('4')
+console.log('5')
+console.log('6')
